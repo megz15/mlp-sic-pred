203bffae7e5e2864afa66ad6fe0b855d4b485bc6
megz15 <meghraj.g16@gmail.com>
2025-11-18 16:15:58 +0530

    feat: compare models

---
+++ compare_models.py
@@ -0,0 +1,22 @@
+import json, os
+
+def calc_model_score(rmse, mae, corr):
+    return (1 / rmse) + (1 / mae) + (corr * 10)
+
+
+model_names = [v.split("_meta.json")[0] for v in os.listdir("metadata")]
+model_metadata = {}
+
+for model in model_names:
+    with open(f"metadata/{model}_meta.json", "r") as f:
+        model_metadata[model] = json.load(f)
+
+for model in model_metadata:
+    rmse = model_metadata[model]["rmse"]
+    mae = model_metadata[model]["mae"]
+    corr = model_metadata[model]["corr"]
+    
+    score = calc_model_score(rmse, mae, corr)
+    model_metadata[model]["score"] = score
+
+print(json.dumps(model_metadata, indent=4))
